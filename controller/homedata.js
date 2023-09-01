977b161c3b1768e4da6c73db25efa8f4fc942c92
Siddh12 <sid.singh9099@gmail.com>
2023-09-01 12:42:26 +0530

    For deployment v1.0

--- controller/homedata.js
+++ controller/homedata.js
@@ -1,40 +1,60 @@
 import nodemailer from 'nodemailer';
-import{mongoose}  from 'mongoose';
-const {Schema} = mongoose;
+import { mongoose } from 'mongoose';
+const { Schema } = mongoose;
+import jwt from 'jsonwebtoken';
+
+
+// connect mongoAtls (cloud)
+
+mongoose.connect(
+  "mongodb+srv://SiddhSpotBuy:SpotBuy@cluster0.fo0wwv7.mongodb.net/?retryWrites=true&w=majority",
+  {useNewUrlParser:true})
+  .then(()=>{
+      console.log("Connected to MongoDB");
+  })
+  .catch(()=>{
+      console.log("Couldn't connect to MongoDB");
+  })
+
+// connect mongoAtls (cloud) end 
 
 
 
 
 
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/spotBuy');
-  console.log("DataBase is connected");
-}
-
-main().catch(err => console.log(err));
+// connect to localdatabase 
 
 
+
+// async function main() {
+//   await mongoose.connect('mongodb://127.0.0.1:27017/spotBuy');
+//   console.log("DataBase is connected");
+// }
+
+// main().catch(err => console.log(err));
+
+// connect to local database 
 
 const ProductSchema = new Schema({
-    user_id: Number,
-    category_id: Number,
-    brand_id: Number,
-    model_id: Number,
-    trim_id: Number,
-    fuelType: Number,
-    modelYear : Number,
-    country_id: Number,
-    state_id: Number,
-    city_id:Number,
-    postStartDateTime:{ type: Date, default: Date.now },
-    postEndDateTime: {type: Date},
-    title : String,
-    Image: [String],
-    description :String,
-    price : Number,
-    transmissionType : String,
-    owner : Number,
-    status: Boolean
+  user_id: Number,
+  category_id: Number,
+  brand_id: Number,
+  model_id: Number,
+  trim_id: Number,
+  fuelType: Number,
+  modelYear: Number,
+  country_id: Number,
+  state_id: Number,
+  city_id: Number,
+  postStartDateTime: { type: Date, default: Date.now },
+  postEndDateTime: { type: Date },
+  title: String,
+  Image: [String],
+  description: String,
+  price: Number,
+  transmissionType: String,
+  owner: Number,
+  status: Boolean
 });
 
 
@@ -44,31 +64,26 @@ const Product = mongoose.model('Product', ProductSchema)
 
 
 const userSchema = new Schema({
-    otp : Number,
-    name: String,
-    username: String,
-    mobile_No: Number,
-    alternate_mobile: Number,
-    datetime: { type: Date, default: Date.now },
-    email: String,
-    gender: String,
-    age: Number,
-    status: Boolean,
-    image: [String],
-    availablePost: Number,
-    followers: [Number],
-    following: [Number],
-    address: {type: Object}
+  otp: Number,
+  name: String,
+  username: String,
+  mobile_No: Number,
+  alternate_mobile: Number,
+  datetime: { type: Date, default: Date.now },
+  email: String,
+  gender: String,
+  age: Number,
+  status: Boolean,
+  image: [String],
+  availablePost: Number,
+  followers: [Number],
+  following: [Number],
+  address: { type: Object },
+  token: String
 
 });
 
 const User = mongoose.model('User', userSchema)
-
-
-
-
-
-
 
 
 
@@ -78,8 +93,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-      user: 'sid.singh9099@gmail.com',
-      pass: 'kxzsqrmtzxlafsop'
+    user: 'sid.singh9099@gmail.com',
+    pass: 'kxzsqrmtzxlafsop'
   }
 });
 
@@ -87,97 +102,179 @@ const transporter = nodemailer.createTransport({
 
 class UserController {
 
-//  whene user will be add new product
+  //  whene user will be add new product
 
   static CreateNewProduct = async (req, res) => {
     const product = new Product(req.body);
     console.log(req.body);
-    var databaseres =  await product.save()
+    var databaseres = await product.save()
     console.log(databaseres);
     res.status(201).json(databaseres)
   }
-  
-// For verification email   
 
-  static email = async function (req, res) {
+  // For email verification email   
+
+  static emailvarification = async function (req, res) {
+
 
     function otpfunction() {
       return Math.floor(100000 + Math.random() * 900000);
     }
-    var otp = otpfunction()
-    let data = req.body ;
+    
+    let otp = otpfunction()
+    var databaseresponce
+    var isEmailPresent = [];
+    const checkuserexist = async function () {
+      isEmailPresent = await User.find(req.body).exec();
+      console.log(isEmailPresent.length);
+      // res.status(200).send(isEmailPresent);
+      if (isEmailPresent.length === 0) {
+        console.log(true);
+
+        createnewuser()
+        sendEmail()
+      } else {
+        console.log(false);
+        console.log(isEmailPresent);
+        var alluser = async () => {
+          const allProduct = await User.findByIdAndUpdate(isEmailPresent[0]._id, { $set: { otp: otp } })
+          console.log(allProduct);
+          databaseresponce = allProduct
+        }
+        sendEmail()
+        alluser()
+        setTimeout(() => {
+          var alluser = async () => {
+            const allProduct = await User.findByIdAndUpdate(isEmailPresent[0]._id, { $set: { otp: 111111 } })
+            console.log(allProduct);
+          }
+          alluser()
+        }, 300000);
+      }
+    }
+
+    checkuserexist()
+
+
+
+
+
+    let data = req.body;
     // console.log(data);
     data.otp = otp;
     // console.log(data);
 
-    
+    // console.log(req.body);
 
-    const user = new User(data);
-    var databaseresponce =  await user.save()
-    console.log(databaseresponce);
-    console.log(databaseresponce._id);
-    
-   setTimeout(() => {
-    var alluser = async () => {
-      var id = "64e25834a5c5d1b44d66777a"
-      const allProduct = await User.findByIdAndUpdate(databaseresponce._id, { $set: {otp: 111111}})
-       console.log(allProduct);
-     }
-     alluser()
-   }, 300000);
 
-    const info = await transporter.sendMail({
+
+    var createnewuser = async () => {
+
+      const user = new User(data);
+      databaseresponce = await user.save()
+      console.log(databaseresponce);
+      console.log(databaseresponce._id);
+
+      setTimeout(() => {
+        var alluser = async () => {
+          var id = "64e25834a5c5d1b44d66777a"
+          const allProduct = await User.findByIdAndUpdate(databaseresponce._id, { $set: { otp: 111111 } })
+          console.log(allProduct);
+        }
+        alluser()
+      }, 300000);
+    }
+
+    // const user = new User(data);
+    // var databaseresponce = await user.save()
+    // console.log(databaseresponce);
+    // console.log(databaseresponce._id);
+
+    // setTimeout(() => {
+    //   var alluser = async () => {
+    //     var id = "64e25834a5c5d1b44d66777a"
+    //     const allProduct = await User.findByIdAndUpdate(databaseresponce._id, { $set: { otp: 111111 } })
+    //     console.log(allProduct);
+    //   }
+    //   alluser()
+    // }, 300000);
+
+
+    var sendEmail = async () => {
+      const info = await transporter.sendMail({
         from: '"SpotBuy" <sid.singh9099@gmail.com>',
-        to: data.email, 
-        subject: "Login OTP", 
+        to: data.email,
+        subject: "Login OTP",
         text: "Hello",
         html: `
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <meta charset="utf-8">
-          </head>
-          <body>
-          <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;      line-height:2">
-          <div style="margin:50px auto;width:70%;padding:20px 0">
-            <div style="border-bottom:1px solid #eee">
-              <a href="https://spotrental.in/" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">SpotBuy</a>
+            <!DOCTYPE html>
+            <html>
+              <head>
+                <meta charset="utf-8">
+              </head>
+              <body>
+              <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;      line-height:2">
+              <div style="margin:50px auto;width:70%;padding:20px 0">
+                <div style="border-bottom:1px solid #eee">
+                  <a href="https://spotrental.in/" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">SpotBuy</a>
+                </div>
+                <p style="font-size:1.1em">Hello There,</p>
+                <p>Thank you for choosing SpotBuy. Use the following OTP to complete your Sign in procedures. OTP is valid for 5 minutes</p>
+                <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
+                <p style="font-size:0.9em;">Regards,</p>
+                <p style="font-size:0.5em;">SpotBuy</p>
+                <hr style="border:none;border-top:1px solid #eee" />
+                <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+                  <p>SpotBuy</p>
+                  <p>Work From Home DevMode</p>
+                  <p>INDIA</p>
+                </div>
+              </div>
             </div>
-            <p style="font-size:1.1em">Hello There,</p>
-            <p>Thank you for choosing SpotBuy. Use the following OTP to complete your Sign in procedures. OTP is valid for 5 minutes</p>
-            <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
-            <p style="font-size:0.9em;">Regards,</p>
-            <p style="font-size:0.5em;">SpotBuy</p>
-            <hr style="border:none;border-top:1px solid #eee" />
-            <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-              <p>SpotBuy</p>
-              <p>Work From Home DevMode</p>
-              <p>INDIA</p>
-            </div>
-          </div>
-        </div>
-          </body>
-        </html>
-      `
-    });
+              </body>
+            </html>
+          `
+      });
 
-    var resval = {
+      var resval = {
         "otpinformation": info,
         "Databaseinformation": databaseresponce
+      }
+      res.status(200).send(resval);
+      console.log("Message sent: %s", info.messageId);
+      console.log(info);
     }
-    res.status(200).send(resval);
-    console.log("Message sent: %s", info.messageId);
-    console.log(info);
+
+
+  }
+  // for OTP varification 
+
+  static otpVarification = async (req, res) => {
+    console.log(req.body);
+    const gototpdetails = await User.findById(req.body.id).exec();
+    // console.log(gototpdetails);
+    if (gototpdetails.otp === req.body.otp) {
+      let token = jwt.sign({ email: gototpdetails.email }, 'AAAAAAAAAA')
+      let databasevaluewithtoken = await User.findByIdAndUpdate(req.body.id, { $set: { token: token } })
+      console.log(databasevaluewithtoken);
+      res.status(202).json({
+        "text": "OTP Verified Successfully",
+        "token": token
+      });
+    } else {
+      res.status(202).json({ "text": "OTP Verified UnSuccessfully" });
+    }
   }
 
+
   // Update user Details 
- 
+
   static updateUserDetails = async (req, res) => {
     console.log(req.body);
     var id = "64e2170b44b14e9521fccac3"
-    const updatedUserDetail = await User.findByIdAndUpdate(req.body.id, { $set: req.body})
-     res.status(202).json(updatedUserDetail)
-   }
+    const updatedUserDetail = await User.findByIdAndUpdate(req.body.id, { $set: req.body })
+    res.status(202).json(updatedUserDetail)
+  }
 
 
 
@@ -186,7 +283,7 @@ class UserController {
 
 
   static allProduct = async (req, res) => {
-   const allProduct = await Product.find()
+    const allProduct = await Product.find()
     res.status(202).json(allProduct)
   }
 
@@ -256,7 +353,7 @@ class UserController {
 
 
 
-   // working email function end 
+  // working email function end 
 }
 
 
--- routes/routes.js
+++ routes/routes.js
@@ -4,25 +4,30 @@ import userControllers from '../controller/homedata.js'
 router.use(express.json())
 import cors from 'cors'
 const corsOpts = {
-    origin: '*',
-  
-    methods: [
-      'GET',
-      'POST',
-    ],
-  
-    allowedHeaders: [
-      'Content-Type',
-    ],
-  };
-  
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
 router.use(cors(corsOpts));
+
+
+
 // public Routes 
 
 router.post('/CreateNewProduct', userControllers.CreateNewProduct);
-router.post('/email', userControllers.email);
+router.post('/emailvarification', userControllers.emailvarification);
 router.get('/allproduct', userControllers.allProduct);
-router.post('/updateUserDetails', userControllers.updateUserDetails)
+router.post('/updateUserDetails', userControllers.updateUserDetails);
+router.post('/otpVarification', userControllers.otpVarification);
+
 
 
 export default router
